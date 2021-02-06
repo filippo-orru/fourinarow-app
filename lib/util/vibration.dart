f3d7ee96a3121b1bdc907e0fbab3114cd2451fc0
Filippo Orru <filippoorru@hotmail.com>
2021-02-06 13:29:58 +0100

    Check vibration for web compatibility

--- lib/util/vibration.dart
+++ lib/util/vibration.dart
@@ -2,6 +2,8 @@ import 'package:flutter_vibrate/flutter_vibrate.dart';
 
 class Vibrations {
   static void win() async {
+    if (!await Vibrate.canVibrate) return;
+
     Vibrate.feedback(FeedbackType.medium);
     await Future.delayed(Duration(milliseconds: 160));
     Vibrate.feedback(FeedbackType.medium);
@@ -10,24 +12,34 @@ class Vibrations {
   }
 
   static void loose() async {
+    if (!await Vibrate.canVibrate) return;
+
     Vibrate.feedback(FeedbackType.medium);
     await Future.delayed(Duration(milliseconds: 160));
     Vibrate.feedback(FeedbackType.medium);
   }
 
   static void turnChange() async {
+    if (!await Vibrate.canVibrate) return;
+
     Vibrate.feedback(FeedbackType.light);
   }
 
   static void tiny() async {
+    if (!await Vibrate.canVibrate) return;
+
     Vibrate.feedback(FeedbackType.light);
   }
 
   static void battleRequest() async {
+    if (!await Vibrate.canVibrate) return;
+
     Vibrate.feedback(FeedbackType.medium);
   }
 
   static void gameFound() async {
+    if (!await Vibrate.canVibrate) return;
+
     await Future.delayed(Duration(milliseconds: 90));
     Vibrate.feedback(FeedbackType.light);
     await Future.delayed(Duration(milliseconds: 60));
